dd17a050258a5e92a25cda7b29271bae937b1766
Rizki Ahmad Gufron <at113928@gmail.com>
2025-05-16 21:18:57 +0700

    add enemy movement
    now enemy can patrol and chase player when around

--- Assets/enemy1logic.cs
+++ Assets/enemy1logic.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class enemy1logic : MonoBehaviour
@@ -7,6 +8,11 @@ public class enemy1logic : MonoBehaviour
     public Transform checkpoint;
     public float distance = 1f;
     public LayerMask layerMask;
+    public Animator animator;
+    public Transform player;
+    public float visionRange = 10f;
+    public float attackRange = 2f;
+    public bool inRange = false;
     void Start()
     {
         
@@ -15,19 +21,57 @@ public class enemy1logic : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
-        RaycastHit2D hit = Physics2D.Raycast(checkpoint.position, Vector2.left, distance, layerMask);
+        if (Vector2.Distance(transform.position, player.position) <= visionRange)
+        {
+            inRange = true;
+            if (player.position .x < transform.position.x && facingLeft == false)
+            {
+                transform.eulerAngles = new Vector3(0f, 0f, 0f);
+                transform.position = new Vector3(transform.position.x - 4f, transform.position.y, 0f);
+                facingLeft = true;
+            }
+            else if (player.position.x > transform.position.x && facingLeft == true)
+            {
+                transform.eulerAngles = new Vector3(0f, -180f, 0f);
+                transform.position = new Vector3(transform.position.x + 4f, transform.position.y, 0f);
+                facingLeft = false;
+            }   
+            if (Vector2.Distance(transform.position, player.position) <= attackRange)
+            {
+                // Attack the player
+                Debug.Log("Attack the player");
+                animator.SetBool("attack1", true);
+                
+            }
+            else
+            {
+                animator.SetBool("attack1", false);
+                transform.position = Vector2.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
+            }
+        }
+        else
+        {
+            inRange = false;
+            transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+            RaycastHit2D hit = Physics2D.Raycast(checkpoint.position, Vector2.down, distance, layerMask);
 
-        if (hit == false && facingLeft == true)
-        {
-            transform.eulerAngles = new Vector3(0f, -180f, 0f);
-            facingLeft = false;
+            if (hit == false && facingLeft == true)
+            {
+                transform.eulerAngles = new Vector3(0f, -180f, 0f);
+                transform.position = new Vector3(transform.position.x + 4f, transform.position.y, 0f);
+                facingLeft = false;
+            }
+            else if (hit == false && facingLeft == false)
+            {
+                transform.eulerAngles = new Vector3(0f, 0f, 0f);
+                transform.position = new Vector3(transform.position.x - 4f, transform.position.y, 0f);
+                facingLeft = true;
+            }
         }
-        else if (hit == true && facingLeft == false)
-        {
-            transform.eulerAngles = new Vector3(0f, 0f, 0f);
-            facingLeft = true;
-        }
+        
+
+       
+
     }
 
     private void OnDrawGizmos()
@@ -37,6 +81,8 @@ public class enemy1logic : MonoBehaviour
         Gizmos.color = Color.red;
         Gizmos.DrawRay(checkpoint.position, Vector2.left * distance);
 
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, visionRange);
     }
 
     
